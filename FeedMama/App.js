96a8dc4fbce68952fa558024c5b7589f48824843
CKneeland <Connor.Kneeland@marquette.edu>
2022-03-04 13:55:12 -0600

    LoginScreen Update
    Created login screen UI

--- FeedMama/App.js
+++ FeedMama/App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, StyleSheet, TouchableOpacity, View, TextInput, Image } from 'react-native';
+//import LoginScreen from './screens/LoginScreen';
+//import SignUpUserTypeScreen from'.screens/SignUpUserTypeScreen'
+//import { NavigationContainer } from '@react-navigation/native';
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  //return <LoginScreen/>
+  return(
+    <ImageBackground
+      source={require("../FeedMama/app/assets/initialbackground.jpg")}
+      resizeMode="cover"
+      style={styles.background}>
+
+        <Image
+          source={require("../FeedMama/app/assets/FeedMamaWhiteLogo.jpg")}
+          resizeMode="contain"
+          style={styles.logo}
+        ></Image>
+
+        <View
+          style={styles.container}>
+
+        <TextInput style={styles.input} placeholder={"Username"}/>
+        <TextInput style={styles.input} placeholder={"Password"}/>
+
+          <TouchableOpacity>
+
+            <ImageBackground
+              style={{
+                width: 200,
+                height: 58,
+                marginTop: "5%"
+              }}
+              source={require("../FeedMama/app/assets/WhiteLoginButton.jpg")}
+              resizeMode="contain">
+
+            </ImageBackground>
+
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+
+            <ImageBackground
+              style={{
+                width: 200,
+                height: 58,
+                marginTop: "2%",
+              }}
+              source={require("../FeedMama/app/assets/WhiteSignUpButton.jpg")}
+              resizeMode="contain">
+
+            </ImageBackground>
+
+          </TouchableOpacity>
+
+        </View>
+
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: "#E6E6E6",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  input: {
+    width: 265,
+    height: 30,
+    backgroundColor: "rgba(255,255,255,1)",
+    borderRadius: 12,
+    marginBottom: "5%",
+    paddingLeft: 10
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginTop: "30%",
+    marginBottom: "-50%",
   },
 });
---
+++ FeedMama/screens/LoginScreen.js
@@ -0,0 +1,140 @@
+import React, { Component } from "react";
+//import { NavigationContainer } from '@react-navigation/native';
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import {
+  StyleSheet,
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  TextInput
+} from "react-native";
+
+function LoginScreen(props) {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../app/assets/initialbackground.jpg")}
+        resizeMode="contain"
+        style={styles.image}
+        imageStyle={styles.image_imageStyle}
+      >
+        <Image
+          source={require("../app/assets/FeedMamaWhiteLogo.jpg")}
+          resizeMode="contain"
+          style={styles.image2}
+        ></Image>
+        <TextInput style={styles.rect2} placeholder={"Username"}/>
+        <TextInput style={styles.rect3} placeholder={"Password"}/>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("SignUpUserTypeScreen")}
+            style={styles.button}
+          ></TouchableOpacity>
+          <Text style={styles.staySignedIn}>Stay Signed In</Text>
+        </View>
+        <ImageBackground
+          source={require("../app/assets/WhiteLoginButton.jpg")}
+          resizeMode="contain"
+          style={styles.image3}
+          imageStyle={styles.image3_imageStyle}
+        >
+          <TouchableOpacity
+            onPress={() => props.navigation.navigate("LoginScreen")}
+            style={styles.button2}
+          ></TouchableOpacity>
+        </ImageBackground>
+        <Image
+          source={require("../app/assets/WhiteSignUpButton.jpg")}
+          resizeMode="contain"
+          style={styles.image4}
+        ></Image>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    zIndex: -1
+  },
+  image: {
+    width: 757,
+    height: 812,
+    marginLeft: -191
+  },
+  image_imageStyle: {},
+  image2: {
+    width: 200,
+    height: 200,
+    marginTop: 86,
+    marginLeft: 279
+  },
+  rect2: {
+    width: 265,
+    height: 30,
+    backgroundColor: "rgba(255,255,255,1)",
+    borderRadius: 12,
+    marginTop: 73,
+    marginLeft: 246,
+    paddingLeft: 10
+  },
+  rect3: {
+    width: 265,
+    height: 30,
+    backgroundColor: "rgba(255,255,255,1)",
+    borderRadius: 12,
+    marginTop: 17,
+    marginLeft: 246,
+    paddingLeft: 10
+  },
+  button: {
+    width: 16,
+    height: 16,
+    backgroundColor: "rgba(255,255,255,1)"
+  },
+  staySignedIn: {
+    fontFamily: "be-vietnam-pro-regular",
+    color: "rgba(255,255,255,1)",
+    fontSize: 11,
+    marginLeft: 14,
+    marginTop: 2
+  },
+  buttonRow: {
+    height: 16,
+    flexDirection: "row",
+    marginTop: 24,
+    marginLeft: 311,
+    marginRight: 341
+  },
+  image3: {
+    width: 200,
+    height: 58,
+    marginTop: 16,
+    marginLeft: 279
+  },
+  image3_imageStyle: {},
+  button2: {
+    width: 200,
+    height: 58,
+    backgroundColor: "#E6E6E6",
+    opacity: 0
+  },
+  image4: {
+    width: 200,
+    height: 56,
+    marginTop: 12,
+    marginLeft: 279
+  }
+});
+
+export default LoginScreen;
