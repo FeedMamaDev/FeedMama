668b632b93cace5585481a7364f107c27bb5400f
CKneeland <60235610+CKneeland@users.noreply.github.com>
2022-03-21 16:31:48 -0500

    Revert "More Navigation"
    This reverts commit 905b1242babdebcf1403cd63fb93086c81118921.

--- FeedMama/screens/SignUpAcctInfoScreen.js
+++ FeedMama/screens/SignUpAcctInfoScreen.js
@@ -93,7 +93,7 @@ function SignUpAcctInfoScreen(props){
           onChangeText={text => setPhoneNumber(text)}
         />
 
-        <TouchableOpacity onPress={() => props.navigation.navigate("Home")}>
+        <TouchableOpacity onPress={() => PrintEP()}>
 
           <ImageBackground
             style={styles.primaryButton}
@@ -123,6 +123,22 @@ function SignUpAcctInfoScreen(props){
 
           </TouchableOpacity>
 
+          <TouchableOpacity
+            style={{
+              marginLeft: "15%",
+            }} onPress={() => printUserType()}>
+          
+            <ImageBackground
+                style={{
+                  width: 80,
+                  height: 80
+                }}
+                source={require("../app/assets/Buttons/ImaPhysician.png")}
+                resizeMode="contain">
+
+            </ImageBackground>
+
+          </TouchableOpacity>
         </View>
 
     </View>
