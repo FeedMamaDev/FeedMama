c16dddd60026c67af711c160b8064cf298e01a9b
CKneeland <Connor.Kneeland@marquette.edu>
2022-03-28 14:55:08 -0500

    SearchScreen Update
    - Don't pay attention to DonateScreen

--- screens/DonateScreen.js
+++ screens/DonateScreen.js
@@ -1,14 +1,91 @@
 import React, {useState} from 'react'
-import { View, Text} from 'react-native';
+import { View, Text, Image, StyleSheet} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+
+
+function updateDonation(input, current) {
+    if(input === "0" & current === "0"){ //Initial value
+        return "0";
+    }
+    else if(input === "delete"){
+        return "delete";
+    }
+    else{
+        return current.concat(input);
+    }
+}
 
 const DonateScreen = ({navigation}) => {
+    let Donation = updateDonation("0","0")
     return (
-        <View>
-            <Text>
-                Donate
-            </Text>
+        <View style={{backgroundColor: "#fff", paddingTop: 50, alignItems: 'center'}}>
+            <Image source={require("../app/assets/Static/FeedMamaSecLogo.png")} resizeMode="contain"/>
+            <Text style= {{fontSize: 40, marginTop: "5%", marginBottom: "2%"}}>Donate</Text>
+            <View style={{backgroundColor: "#FF6C6C", height: "80%", width: "100%"}}> 
+                <Text style={{color: "white", fontSize:80, marginTop: 10}}>${Donation}</Text>
+                <View style={styles.containerHorz}>
+                    <TouchableOpacity style={{marginRight: "7.5%"}} onPress={() => updateDonation("1", {Donation})}>
+                        <Text style={styles.dimmer}>1</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{marginLeft: "7.5%", marginRight: "7.5%"}}>
+                        <Text style={styles.dimmer}>2</Text>
+                    </TouchableOpacity> 
+                    <TouchableOpacity style={{marginLeft: "7.5%"}}>
+                        <Text style={styles.dimmer}>3</Text>
+                    </TouchableOpacity>     
+                </View>
+                <View style={styles.containerHorz}>
+                    <TouchableOpacity style={{marginRight: "7.5%"}}>
+                        <Text style={styles.dimmer}>4</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{marginLeft: "7.5%", marginRight: "7.5%"}}>
+                        <Text style={styles.dimmer}>5</Text>
+                    </TouchableOpacity> 
+                    <TouchableOpacity style={{marginLeft: "7.5%"}}>
+                        <Text style={styles.dimmer}>6</Text>
+                    </TouchableOpacity>     
+                </View>
+                <View style={styles.containerHorz}>
+                    <TouchableOpacity style={{marginRight: "7.5%"}}>
+                        <Text style={styles.dimmer}>7</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{marginLeft: "7.5%", marginRight: "7.5%"}}>
+                        <Text style={styles.dimmer}>8</Text>
+                    </TouchableOpacity> 
+                    <TouchableOpacity style={{marginLeft: "7.5%"}}>
+                        <Text style={styles.dimmer}>9</Text>
+                    </TouchableOpacity>     
+                </View>
+                <View style={styles.containerHorz}>
+                    <TouchableOpacity style={{marginRight: "7.5%"}}>
+                        <Text style={styles.dimmer}>.</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{marginLeft: "7.5%", marginRight: "7.5%"}}>
+                        <Text style={styles.dimmer}>0</Text>
+                    </TouchableOpacity> 
+                    <TouchableOpacity style={{marginLeft: "7.5%"}}>
+                    <Image source={require("../app/assets/Buttons/DeleteButton.png")} resizeMode="contain"/>
+                    </TouchableOpacity>     
+                </View>
+            </View>
         </View>
     )
 }
 
 export default DonateScreen
+
+const styles = StyleSheet.create({
+    containerHorz: {
+        flex: 1,
+        flexDirection: 'row',
+        marginLeft: "15%",
+        marginRight: "15%",
+        alignItems: "center",
+    },
+    dimmer: {
+        opacity: 75,
+        color: "white",
+        fontSize: 50,
+    }
+});
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,49 +1,39 @@
-import React, {Component}  from 'react';
-import{ StyleSheet, Text, View, FlatList, Image } from 'react-native';
-
+import React, { Component } from "react";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 import { ListItem, SearchBar } from "react-native-elements";
 import filter from "lodash.filter";
-
+  
 const DATA = [
-  {
-    id:"1",
-    title:"Funky Fresh 1"
-  },
-  {
-    id:"2",
-    title:"Funky Fresh 2"
-  },
-  {
-    id:"3",
-    title:"Funky Fresh 3"
-  },
-  {
-    id:"4",
-    title:"Funky Fresh 4"
-  },
+    {
+        id:"1",
+        title:"Funky Fresh 1"
+      },
+      {
+        id:"2",
+        title:"Funky Fresh 2"
+      },
+      {
+        id:"3",
+        title:"Funky Fresh 3"
+      },
+      {
+        id:"4",
+        title:"Funky Fresh 4"
+      },
 ];
   
-const Item = ({title}) => {
-  return( 
-    <View style={styles.card}>
-    <Image style={styles.image} source={require("../app/assets/Photos/FunkyFreshSpringRolls.jpg")}></Image>
-    <View style={styles.detailsContainer}>
-        <Text style={styles.title}>{title}</Text>
+const Item = ({ title }) => {
+    return( 
+        <View style={styles.card}>
+        <Image style={styles.image} source={require("../app/assets/Photos/FunkyFreshSpringRolls.jpg")}></Image>
+        <View style={styles.detailsContainer}>
+            <Text style={styles.title}>{title}</Text>
+        </View>
     </View>
-</View>
-  );
-}
-
-const renderItem = ({item})=>( 
-  <View style={{
-    marginRight: "3%",
-    marginLeft: "3%",
-    marginBottom: "5%"
-  }}>
-    <Item title={item.title}/>
-  </View>
+      );
+};
   
-);
+const renderItem = ({ item }) => <Item title={item.title} />;
 
 class SearchScreen extends Component {
   constructor(props) {
@@ -56,7 +46,7 @@ class SearchScreen extends Component {
     };
     this.arrayholder = DATA;
   }
-
+  
   searchFunction = (text) => {
     const updatedData = this.arrayholder.filter((item) => {
       const item_data = `${item.title.toUpperCase()})`;
@@ -65,11 +55,11 @@ class SearchScreen extends Component {
     });
     this.setState({ data: updatedData, searchValue: text });
   };
-
+  
   render() {
     return (
       <View style={styles.container}>
-        <SearchBar 
+        <SearchBar
           placeholder="Search Here..."
           lightTheme
           round
@@ -77,52 +67,59 @@ class SearchScreen extends Component {
           onChangeText={(text) => this.searchFunction(text)}
           autoCorrect={false}
         />
-       <View style={styles.container}>
-        <FlatList
-          data={DATA}
+        <FlatList 
+          data={this.state.data}
           renderItem={renderItem}
-          keyExtractor={item => item.id}
+          keyExtractor={(item) => item.id}
           numColumns={2}
+          columnWrapperStyle={styles.row}
+          style={{
+              width: "100%",
+              height: "100%",
+              marginTop: 20
+          }}
         />
-        </View>
       </View>
     );
   }
 }
-
   
 export default SearchScreen;
-
+  
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
-    padding: 5,
-  },
-  card:{
-    borderRadius: 15,
-    backgroundColor: "#ff6c6c",
-    marginBottom: "5%",
-    overflow: 'hidden'
-  },
-  detailsContainer:{
-      padding: 20,
-  },
-  image:{
-      width:"100%",
-      height:150
-  },
-  title:{
-      fontSize:14,
-      fontFamily: Platform.OS === "iOS" ? "Proxima Nova" : "Helvetica",
-      fontWeight:"bold",
-      color: "#fff",
-      marginLeft: 5
-  },
-  containerHorz: {
-    flex: 1,
-    flexDirection: 'row',
-    marginTop: "5%",
-    marginBottom: "5%",
-    alignItems: "center",
+    container: {
+        marginTop: 50,
+        padding: 5,
+      },
+      row: {
+        flex: 1,
+        justifyContent: "space-around"
+    },
+      card:{
+        borderRadius: 15,
+        backgroundColor: "#ff6c6c",
+        marginBottom: "5%",
+        overflow: 'hidden',
+        flex: .40,
+      },
+      detailsContainer:{
+          padding: 20,
+      },
+      image:{
+          width:"100%",
+          height:150
+      },
+      title:{
+          fontSize:14,
+          fontFamily: Platform.OS === "iOS" ? "Proxima Nova" : "Helvetica",
+          fontWeight:"bold",
+          color: "#fff",
+          marginLeft: 5
+      },
+     item: {
+        backgroundColor: "#f5f520",
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
   },
 });
