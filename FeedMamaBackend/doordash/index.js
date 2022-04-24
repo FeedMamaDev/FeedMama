658cc495a50003fa73ec9e77d11b92bfbd671b00
FeedMamaDev <100809419+FeedMamaDev@users.noreply.github.com>
2022-04-24 16:58:05 -0500

    Implemented in-code JWT generation

--- FeedMamaBackend/doordash/index.js
+++ FeedMamaBackend/doordash/index.js
@@ -3,9 +3,9 @@ const { v4: uuidv4 } = require("uuid");
 const axios = require('axios');
 
 const client = new DoorDashClient.DoorDashClient({
-    "developer_id": "881e3e0b-2ed4-4106-b9d9-4b605b9765fe",
-   "key_id": "3fbe625a-01a3-4376-916c-a5291bced78c",
-   "signing_secret": "0NSaut3rE6idxIWddHQqaMupI_KRUPG2d0vqOLmm9so"
+  "developer_id": "881e3e0b-2ed4-4106-b9d9-4b605b9765fe",
+  "key_id": "8c5d00d7-9dea-4695-9ad0-f4d6dd24406f",
+  "signing_secret": "Y28Q2pbCKUJfuDgH3Qkh8Fb2hptj3nDP91vSAJ0yEAw"
 });
 
 const response = client
@@ -28,8 +28,27 @@ const response = client
     // handle error
   });
 
+function getToken() {
 
-axios.get('https://openapi.doordash.com/drive/v2/deliveries/fe4154a2-0381-4e54-8706-831b2472cfa8', { headers: { 'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImRkLXZlciI6IkRELUpXVC1WMSJ9.eyJhdWQiOiJkb29yZGFzaCIsImlzcyI6Ijg4MWUzZTBiLTJlZDQtNDEwNi1iOWQ5LTRiNjA1Yjk3NjVmZSIsImtpZCI6IjNmYmU2MjVhLTAxYTMtNDM3Ni05MTZjLWE1MjkxYmNlZDc4YyIsImlhdCI6MTY1MDU4OTgyMSwiZXhwIjoxNjUwNTkxNjIxfQ.9u7R-yr1mkbDxeqAJmFqBQ2FRA785dQAMZD1VT3GmO8', 'Content-Type': 'application/json' } })
+  const jwt = require('jsonwebtoken');
+
+  const data = {
+      "aud": "doordash",
+      "iss": "881e3e0b-2ed4-4106-b9d9-4b605b9765fe", 
+      "kid": "8c5d00d7-9dea-4695-9ad0-f4d6dd24406f", 
+      "exp": Math.floor((Date.now() / 1000) + 60),
+      "iat": Math.floor(Date.now() / 1000),
+  };
+
+  const headers = { algorithm: 'HS256', header: { 'dd-ver': 'DD-JWT-V1' } }
+
+  const token = jwt.sign(data, Buffer.from("Y28Q2pbCKUJfuDgH3Qkh8Fb2hptj3nDP91vSAJ0yEAw" , 'base64'), headers);
+
+  return token;
+
+}
+
+axios.get('https://openapi.doordash.com/drive/v2/deliveries/' + "7c06ab62-540a-4ae4-ad47-af598e4b5700", { headers: { 'Authorization': 'Bearer ' + getToken(), 'Content-Type': 'application/json' } })
     .then(function (response) {
         console.log(response.data);
     })
